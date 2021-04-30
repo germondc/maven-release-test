33308439a0897613555a68c57c9bba34cc463ee1
Clyde Germond <clyde.germond@gmail.com>
2021-04-30 19:26:49 +0200

    first commit

---
+++ src/main/java/test/clyde/maven/release/MaveReleaseTestApplication.java
@@ -0,0 +1,13 @@
+package test.clyde.maven.release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaveReleaseTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaveReleaseTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/clyde/maven/release/MaveReleaseTestApplicationTests.java
@@ -0,0 +1,13 @@
+package test.clyde.maven.release;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaveReleaseTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
